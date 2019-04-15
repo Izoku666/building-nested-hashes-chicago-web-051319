5312f9ad76939dfb46c018f747bfddc2336f5e4c
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-15 23:50:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,8 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
-      :hero_friends => [{}{}]
       }, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => [{}{}]
       }}
 
   
